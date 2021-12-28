d1cfd69c2f81e3d2ed68396ccb1939b77143bb13
Jamie <jamie1997120@gmail.com>
2021-12-28 23:46:27 +0800

    adds text a picture a button and a link to another page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import redPicture from './assets/red-picture.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Jamie Z</h1>
+      <p>I think technology should not be created because of demand, as it’s my belief that it will greatly limit the possibility of a technology explosion. I strongly believe that technology precedes application scenarios. So, it is my hope that my participation in relevant research projects will contribute to creating a set of rational interaction logic through design to create application scenarios for technology that people really need.
+ </p>
+<img src={redPicture} alt="red-picture" />
+<a href="https://jamiezhao.site/"><button className="button">click portfolio</button></a>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
