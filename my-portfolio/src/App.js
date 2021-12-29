aac656afec4ef5be8c186d41f45bd7671f135c51
Jamie <jamie1997120@gmail.com>
2021-12-29 21:20:31 +0800

    changes picture size

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,10 +6,17 @@ function App() {
   return (
     <div className="App">
       <h1>Jamie Z</h1>
-      <p>I think technology should not be created because of demand, as it’s my belief that it will greatly limit the possibility of a technology explosion. I strongly believe that technology precedes application scenarios. So, it is my hope that my participation in relevant research projects will contribute to creating a set of rational interaction logic through design to create application scenarios for technology that people really need.
- </p>
-<img src={redPicture} alt="red" />
-<a href="https://jamiezhao.site/"><button className="button">click portfolio</button></a>
+      <div>
+      <p>I think technology should not be created because of demand, as it’s my belief that it will greatly limit the possibility of a technology explosion. I strongly believe that technology precedes application scenarios. So, it is my hope that my participation in relevant research projects will contribute to creating a set of rational interaction logic through design to create application scenarios for technology that people really need.</p>
+      </div>
+<div className='imgWrap'>
+<a href="https://jamiezhao.site/">
+  <img className='img' src={redPicture} alt="red" />
+</a>
+</div>
+<a href="https://jamiezhao.site/">
+  <button className="button">click portfolio</button>
+  </a>
       <header className="App-header">
       </header>
     </div>
