dc0c035a087abca4ba999cebd295d34351e4c0a5
Jamie <jamie1997120@gmail.com>
2021-12-30 00:30:12 +0800

    adds a twitter and behance icon and link functionality; rewrite app.js and app.css code.

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,26 @@
-// import logo from './logo.svg';
-import redPicture from './assets/red-picture.png'
 import './App.css';
-
+import picture from './assets/mainpicture.png';
+import {ReactComponent as BehanceLogo} from './assets/behance.svg';
+import {ReactComponent as TwitterLogo} from './assets/twitter.svg';
 function App() {
   return (
     <div className="App">
       <h1>Jamie Z</h1>
-      <div>
       <p>I think technology should not be created because of demand, as it’s my belief that it will greatly limit the possibility of a technology explosion. I strongly believe that technology precedes application scenarios. So, it is my hope that my participation in relevant research projects will contribute to creating a set of rational interaction logic through design to create application scenarios for technology that people really need.</p>
+      <a href="https://jamiezhao.site/" >
+        <img src={picture} alt="3 women looking through a window and a red staircase" />
+      </a>
+      <a href="https://jamiezhao.site/" >
+        <button className="portfolio-button">Check Portfolio</button>
+      </a>
+      <div className="icon-wrapper">
+      <a href="https://www.behance.net/msjamie/moodboards" >
+        <BehanceLogo className="behance-logo"/>
+      </a>
+      <a href="https://twitter.com/amyzxq1997120" >
+        <TwitterLogo className="twitter-logo"/>
+      </a>
       </div>
-<div className='imgWrap'>
-<a href="https://jamiezhao.site/">
-  <img className='img' src={redPicture} alt="red" />
-</a>
-</div>
-<a href="https://jamiezhao.site/">
-  <button className="button">click portfolio</button>
-  </a>
-      <header className="App-header">
-      </header>
     </div>
   );
 }
