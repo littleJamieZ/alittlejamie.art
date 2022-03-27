1565f23caf7040d4fd0c81a93265d4420534c8ff
Jamie <jamie1997120@gmail.com>
2022-03-27 14:33:00 -0400

    2:30 PM Deletes everything in App.js and CSS; installs scss, creates the structure of the website on App.js; adds a header, CTA, and footer, and creates class-name for these three sections

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,34 +1,16 @@
-import './App.css';
-import picture from './assets/mainpicture.png';
-import {ReactComponent as BehanceLogo} from './assets/behance.svg';
-import {ReactComponent as TwitterLogo} from './assets/twitter.svg';
+import './App.scss';
+import { ReactComponent as BehanceLogo } from './assets/behance.svg';
+import { ReactComponent as TwitterLogo } from './assets/twitter.svg';
 function App() {
-  return (
-    <div className="App">
-      <div className="text-wrapper">
-        <h1>Jamie Z</h1>
-        <p>I think technology should not be created because of demand, as it’s my belief that it will greatly limit the possibility of a technology explosion. I strongly believe that technology precedes application scenarios. So, it is my hope that my participation in relevant research projects will contribute to creating a set of rational interaction logic through design to create application scenarios for technology that people really need.</p>
-      </div>
-      <div className="text-wrapper-bot">
-        <a href="https://jamiezhao.site/" >
-          <img src={picture} alt="3 women looking through a window and a red staircase" />
-        </a>
-        <a href="https://jamiezhao.site/" >
-          <button className="portfolio-button">Check Portfolio</button>
-        </a>
-
-        <div className="icon-wrapper">
-        <a href="https://www.behance.net/msjamie/moodboards" >
-          <BehanceLogo className="behance-logo"/>
-        </a>
-        <a href="https://twitter.com/amyzxq1997120" >
-          <TwitterLogo className="twitter-logo"/>
-        </a>
-      </div>
-      </div>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App_header">
+				<h1>Jamie</h1>
+			</header>
+			<div className="App_CTA"></div>
+			<footer className="App_footer">footer</footer>
+		</div>
+	);
 }
 
 export default App;
