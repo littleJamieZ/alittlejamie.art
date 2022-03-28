eba31061fb5b08d5a496b2beb6ffade28f62ad97
Jamie <jamie1997120@gmail.com>
2022-03-28 12:29:42 -0400

    Adds top-wrapper_text, center-wrapper_button, and bottom-wrapper to App_CTA and adds position, font size, and color

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,7 +7,20 @@ function App() {
 			<header className="App_header">
 				<h1 className="App_header_title">Jamie</h1>
 			</header>
-			<div className="App_CTA"></div>
+			<div className="App_CTA">
+				<div className="App_CTA_top-wrapper">
+					<span className="App_CTA_top-wrapper_text">Hi</span>
+					<span className="App_CTA_top-wrapper_text">I'm am UIUX</span>
+					<span className="App_CTA_top-wrapper_text">designer.</span>
+				</div>
+				<div className="App_CTA_center-wrapper">
+					<button className="App_CTA_center-wrapper_button">
+						View Projects
+					</button>
+					<button className="App_CTA_center-wrapper_button">View Resume</button>
+				</div>
+				<div className="App_CTA_bottom-wrapper">image</div>
+			</div>
 			<footer className="App_footer"></footer>
 		</div>
 	);
