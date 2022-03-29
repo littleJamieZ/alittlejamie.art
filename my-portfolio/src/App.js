bdedc6e2a047b123cb502ca620dec779922f7ff0
Jamie <jamie1997120@gmail.com>
2022-03-28 22:49:48 -0400

    adds link to the button, changes position of text-wrapper, and adds portfolio.svg to bottom-wrapper

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { ReactComponent as BehanceLogo } from './assets/behance.svg';
-import { ReactComponent as TwitterLogo } from './assets/twitter.svg';
+import { ReactComponent as TwitterLogo } from './assets/twitter-icon.svg';
 function App() {
 	return (
 		<div className="App">
@@ -9,17 +9,25 @@ function App() {
 			</header>
 			<div className="App_CTA">
 				<div className="App_CTA_top-wrapper">
-					<span className="App_CTA_top-wrapper_text">Hi</span>
-					<span className="App_CTA_top-wrapper_text">I'm am UIUX</span>
-					<span className="App_CTA_top-wrapper_text">designer.</span>
+					<div className="App_CTA_top-wrapper_text-wrapper">
+						<span className="App_CTA_top-wrapper_text-wrapper_text">Hi</span>
+						<span className="App_CTA_top-wrapper_text-wrapper_text">
+							I'm am UIUX
+						</span>
+						<span className="App_CTA_top-wrapper_text-wrapper_text">
+							designer.
+						</span>
+					</div>
 				</div>
 				<div className="App_CTA_center-wrapper">
-					<button className="App_CTA_center-wrapper_button">
-						View Projects
-					</button>
+					<a href="https://jamiezhao.site/">
+						<button className="App_CTA_center-wrapper_button">
+							View Projects
+						</button>
+					</a>
 					<button className="App_CTA_center-wrapper_button">View Resume</button>
 				</div>
-				<div className="App_CTA_bottom-wrapper">image</div>
+				<div className="App_CTA_bottom-wrapper"></div>
 			</div>
 			<footer className="App_footer"></footer>
 		</div>
